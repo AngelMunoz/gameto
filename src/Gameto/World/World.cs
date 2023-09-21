1625cf825af18da492b7a94c362f440301ea1e99
Angel D. Munoz <scythes.lol@gmail.com>
2023-09-21 11:52:29 -0600

    add world script

---
+++ src/Gameto/World/World.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+using Gameto.Lib.Types;
+
+public partial class World : Node
+{
+	private TileMap? Map { get; set; }
+	private PlayerController? Player { get; set; }
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Map = GetNode<TileMap>("TileMap");
+		Player = GetNode<PlayerController>("Player");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+	}
+}
