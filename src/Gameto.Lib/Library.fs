6432b51bb6a56be2ab89e8371b74b29e2002a028
Angel D. Munoz <scythes.lol@gmail.com>
2023-09-21 22:00:33 -0600

    rename stuff

--- src/Gameto.Lib/Library.fs
+++ src/Gameto.Lib/Library.fs
@@ -2,16 +2,11 @@
 
 open Godot
 
-module Say =
-    open System.Diagnostics
-    let hello name = Debug.WriteLine $"Hello %s{name}"
+module Player =
+    
 
 
-
-module PlayerMovement =
-
-
-    let animateKeyboardMovement (sprite: AnimatedSprite2D option) =
+    let AnimateKeyboardMovement (sprite: AnimatedSprite2D option) =
         match sprite with
         | None -> ()
         | Some sprite ->
@@ -26,13 +21,13 @@ module PlayerMovement =
             else
                 sprite.Stop()
 
-    let moveWithKeyboard (player: CharacterBody2D) (speed: float32) : unit =
+    let MoveWithKeyboard (player: CharacterBody2D, speed: float32) : unit =
 
         let direction = Input.GetVector("move_left", "move_right", "move_up", "move_down")
         player.Velocity <- direction * speed
         player.MoveAndSlide() |> ignore
 
-    let moveWithMouse (player: CharacterBody2D) (target: Vector2) (speed: float32) =
+    let MoveWithMouse (player: CharacterBody2D, target: Vector2, speed: float32) =
         player.Velocity <- player.Position.DirectionTo(target) * speed
 
         if player.Position.DistanceTo(target) > 10.f then
